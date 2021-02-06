c26ef6e5e221ebb3ff67b02e704398b2d4ee5f8c
SangwooPark-Sean <tkddn3367@gmail.com>
2021-02-07 02:19:59 +0900

    weather API

---
+++ bg.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+const IMG_NUMBER = 9
+
+function putImg(imgNum) {
+    const image = new Image();
+    image.src = `/imgs/${imgNum+1}.jpg`;
+    image.classList.add("backgroundImg");
+    body.appendChild(image);
+}
+
+function getRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number
+}
+
+function init() {
+    const randNum = getRandom();
+    putImg(randNum);
+}
+init();
---
+++ weather.js
@@ -0,0 +1,59 @@
+const COORDS = "coords";
+const API_KEY = "85039a0e13d58cad5a9a01ce73613a94";
+
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat, long) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`)
+    .then((result) => {
+        return result.json();
+    })
+    .then((json) => {
+        console.log(json)
+        const temp = json.main.temp;
+        const place = json.name;
+        weather.innerHTML = `${Math.round(temp)}ºC @ ${place}`
+    })
+    
+}
+
+function saveCoords(coords) {
+    localStorage.setItem(COORDS, JSON.stringify(coords));
+}
+
+function handleGeoSuccess(pos) {
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+    const coords = {
+        latitude,
+        longitude
+    };
+    saveCoords(coords)
+    getWeather(latitude, longitude);
+}
+
+function handleGeoFail() {
+    console.log("error");    
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoFail);
+}
+
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        console.log(parseCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+};
+
+init();
